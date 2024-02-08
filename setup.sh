92badc217d3cd65ff5b4cec6c5422c8ea4d370c4
Max-Felix Müller <mxfxmmueller@gmail.com>
2024-02-08 10:27:00 +0100

    create but gitignore python environment

--- setup.sh
+++ setup.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-python3 -m venv pthon_environment
+echo "checking for docker"
+echo "running docker compose"
+echo "creating influx credentials"
+
+echo "creating python environment"
+python3 -m venv python_environment
+
+echo "activating python environment"
+
+echo "installing python requirements"
