1e9d5953d94db6a5de609f3051d10e84c6bde2f1
Max-Felix Müller <mxfxmmueller@gmail.com>
2024-02-08 11:22:01 +0100

    remove docker container and volumes when done

--- remove.sh
+++ remove.sh
@@ -1,1 +1,19 @@
+# The name of the container you want to remove
+container_name="influxdb"
+
+# Stop the container
+docker stop $container_name
+
+# Remove the container
+docker rm $container_name
+
+# List all volumes and find the ones associated with the container
+for volume in $(docker volume ls -q); do
+    # Check if the volume is associated with the container
+    if docker volume inspect $volume | grep -q "$container_name"; then
+        # Remove the volume
+        docker volume rm $volume
+    fi
+done
+
 rm -r ./python_environment
